70d8f68e89fe9799a4e7abeea8df3d3ccd5b200d
Vineet Bansal <vineetbansal@protonmail.com>
2025-11-03 09:29:49 -0500

    Integrated the pypyreeqc development done so far, as the pyEQL-phreeqc package, installed with the "phreeqc" extra.

---
+++ src/pyEQL/pyEQL-phreeqc/ext/iphreeqc-3.8.6-17100/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /media/vineetb/delta/playground/kingsbury/pyEQL/src/pyEQL/pyEQL-phreeqc/ext/iphreeqc-3.8.6-17100
+# Build directory: /media/vineetb/delta/playground/kingsbury/pyEQL/src/pyEQL/pyEQL-phreeqc/ext/iphreeqc-3.8.6-17100/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
